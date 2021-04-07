76a49cae95bb1c2fabd6521fd7aad1c8720c6cb5
Karan Bhogal <kbhog001@wch136-24.cs.ucr.edu>
2021-04-07 16:27:02 -0700

    added changes to main.c to check A0

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: kbhog001
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section:21
+ *	Assignment: Lab #2  Exercise #Prelab
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,10 +14,26 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	
+	unsigned char tmpB = 0x00; //Hold B
+	unsigned char tmpA = 0x00; //Hold A
 
     /* Insert your solution below */
     while (1) {
-
+	//read input
+	tmpA = PINA & 0x01;
+	
+	if (tmpA == 0x01){
+		tmpB = (tmpB & 0xFC) | 0x01; //sets tmpB to bbbbbb01
+	}
+	else{
+		tmpB = (tmpB & 0xFC) | 0x02; //sets tmpB to bbbbbb10
+	}
+	
+	PORTB = tmpB;
     }
-    return 1;
+
+    return 0;
 }
